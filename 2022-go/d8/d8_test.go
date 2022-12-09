507b6e1eaa1114994085822480876b1e65fa6e56
kfigon <kamil.figon@gmail.com>
2022-12-09 16:54:43 +0100

    p1 done

--- 2022-go/d8/d8_test.go
+++ 2022-go/d8/d8_test.go
@@ -1,11 +1,13 @@
 package d8
 
 import (
+	"os"
 	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestExample(t *testing.T) {
@@ -18,11 +20,29 @@ func TestExample(t *testing.T) {
 	t.Run("p1", func(t *testing.T) {
 		assert.Equal(t, 21, p1(data))
 	})
+
+	t.Run("p2", func(t *testing.T) {
+		t.Fail()
+	})
+}
+
+func TestFile(t *testing.T) {
+	raw,err := os.ReadFile("data.txt")
+	require.NoError(t, err)
+
+	data := parse(strings.Split(string(raw), "\r\n"))
+	t.Run("p1", func(t *testing.T) {
+		assert.Equal(t, 1820, p1(data))
+	})
+
+	t.Run("p2", func(t *testing.T) {
+		t.Fail()
+	})
 }
 
 type grid [][]int
 
-func (g grid) col() int {
+func (g grid) cols() int {
 	return len(g[0])
 }
 
@@ -30,8 +50,48 @@ func (g grid) rows() int {
 	return len(g)
 }
 
-func (g grid) el(c,r int) int {
-	return g[c][r]
+func (g grid) el(r,c int) int {
+	return g[r][c]
+}
+
+func (g grid) visibleFromTop(r,c int) bool {
+	tree := g.el(r,c)
+	for row := r-1; row >= 0; row-- {
+		if g.el(row,c) >= tree {
+			return false
+		}
+	}
+	return true
+}
+
+func (g grid) visibleFromBottom(r,c int) bool {
+	tree := g.el(r,c)
+	for row := r+1; row < g.rows(); row++ {
+		if g.el(row,c) >= tree {
+			return false
+		}
+	}
+	return true
+}
+
+func (g grid) visibleFromRight(r,c int) bool {
+	tree := g.el(r,c)
+	for col := c+1; col < g.cols(); col++ {
+		if g.el(r,col) >= tree {
+			return false
+		}
+	}
+	return true
+}
+
+func (g grid) visibleFromLeft(r,c int) bool {
+	tree := g.el(r,c)
+	for col := c-1; col >= 0; col-- {
+		if g.el(r,col) >= tree {
+			return false
+		}
+	}
+	return true
 }
 
 func parse(lines []string) grid {
@@ -48,5 +108,13 @@ func parse(lines []string) grid {
 }
 
 func p1(g grid) int {
-	return 0
+	out := 0
+	for r := 1; r < g.rows()-1; r++ {
+		for c := 1; c < g.cols()-1; c++ {
+			if g.visibleFromLeft(r,c) || g.visibleFromRight(r,c) || g.visibleFromTop(r,c) || g.visibleFromBottom(r,c) {
+				out++
+			}
+		}
+	}
+	return out + (g.cols()-2)*2 + (g.rows())*2 // result + grid
 }
