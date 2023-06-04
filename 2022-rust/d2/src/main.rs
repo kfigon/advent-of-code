48f81e552832402af4282c2f20eb9b75a743a0d0
Kamil <kamil.figon@gmail.com>
2023-06-04 21:16:06 +0200

    rock paper scissors

--- 2022-rust/d2/src/main.rs
+++ 2022-rust/d2/src/main.rs
@@ -8,6 +8,7 @@ const example_data: &'static str = "A Y
 B X
 C Z";
 
+#[derive(Clone)]
 enum Move {
     Rock,
     Paper,
@@ -20,21 +21,46 @@ enum Outcome {
     Draw
 }
 
-fn map_enemy(c: &str) -> Result<Move, String> {
+fn map_enemy(c: &str) -> Result<OpponentMove, String> {
     match c {
-        "A" => Ok(Move::Rock),
-        "B" => Ok(Move::Paper),
-        "C" => Ok(Move::Scissors),
+        "A" => Ok(OpponentMove(Move::Rock)),
+        "B" => Ok(OpponentMove(Move::Paper)),
+        "C" => Ok(OpponentMove(Move::Scissors)),
         _ => Err(format!("invalid enemy move '{}'", c))
     }
 }
 
-fn map_player(c: &str) -> Result<Move, String> {
+fn map_player(c: &str) -> Result<MyMove, String> {
     match c {
-        "X" => Ok(Move::Rock),
-        "Y" => Ok(Move::Paper),
-        "Z" => Ok(Move::Scissors),
+        "X" => Ok(MyMove(Move::Rock)),
+        "Y" => Ok(MyMove(Move::Paper)),
+        "Z" => Ok(MyMove(Move::Scissors)),
         _ => Err(format!("invalid player move '{}'", c))
+    }
+}
+
+fn map_strategy(c: &str) -> Result<WinInstruction, String> {
+    match c {
+        "X" => Ok(WinInstruction(Outcome::Lose)),
+        "Y" => Ok(WinInstruction(Outcome::Draw)),
+        "Z" => Ok(WinInstruction(Outcome::Win)),
+        _ => Err(format!("invalid strategy move '{}'", c))
+    }
+}
+
+fn find_my_move(oppMove: &OpponentMove, strat: &WinInstruction) -> MyMove {
+    match (&oppMove.0, &strat.0) {
+
+        (Move::Rock, Outcome::Win) => MyMove(Move::Paper),
+        (Move::Rock, Outcome::Lose) => MyMove(Move::Scissors),
+
+        (Move::Paper, Outcome::Win) => MyMove(Move::Scissors),
+        (Move::Paper, Outcome::Lose) => MyMove(Move::Rock),
+
+        (Move::Scissors, Outcome::Win) => MyMove(Move::Rock),
+        (Move::Scissors, Outcome::Lose) => MyMove(Move::Paper),
+
+        (_, Outcome::Draw) => MyMove(oppMove.0.clone())
     }
 }
 
@@ -70,35 +96,50 @@ fn score(myMove: &MyMove, outcome: &Outcome) -> i32 {
     moveScore + outcomeScore
 }
 
+
 #[test]
 fn p1_test() {
-    assert_eq!(15, solve_p1(example_data));
-    assert_eq!(14827, solve_p1(&std::fs::read_to_string("data.txt").unwrap()));
+    assert_eq!(Ok(15), solve_p1(example_data));
+    assert_eq!(Ok(14827), solve_p1(&std::fs::read_to_string("data.txt").unwrap()));
 }
 
 #[test]
 fn p2_test() {
-    assert_eq!(12, solve_p2(example_data));
-    assert_eq!(13889, solve_p2(&std::fs::read_to_string("data.txt").unwrap()));
+    assert_eq!(Ok(12), solve_p2(example_data));
+    assert_eq!(Ok(13889), solve_p2(&std::fs::read_to_string("data.txt").unwrap()));
 }
 
+struct WinInstruction(Outcome);
 struct OpponentMove(Move);
 struct MyMove(Move);
 
-fn splt_line(line: &str) -> Result<(OpponentMove, MyMove), String> {
-    let raw: (&str, &str) = line
+fn split(line: &str) -> Result<(&str, &str), String> {
+    line
     .split_once(" ")
-    .ok_or(format!("Invalid line: {}", line))?;
+    .ok_or(format!("Invalid line: {}", line))
+}
+
+fn splt_line_p1(line: &str) -> Result<(OpponentMove, MyMove), String> {
+    let raw = split(line)?;
 
     let enemy = map_enemy(raw.0)?;
     let my = map_player(raw.1)?;
 
-    Ok((OpponentMove(enemy), MyMove(my)))
+    Ok((enemy, my))
 }
 
-fn solve_p1(data: &str) -> i32 {
+fn splt_line_p2(line: &str) -> Result<(OpponentMove, WinInstruction), String> {
+    let raw = split(line)?;
+
+    let enemy = map_enemy(raw.0)?;
+    let strat = map_strategy(raw.1)?;
+
+    Ok((enemy, strat))
+}
+
+fn solve_p1(data: &str) -> Result<i32, String> {
     let res: Result<Vec<i32>, String> = data.lines()
-    .map(splt_line)
+    .map(splt_line_p1)
     .map(|v| v.map(|vals| {
         let out = game(&vals.1, &vals.0);
         (vals.1, out)
@@ -106,15 +147,22 @@ fn solve_p1(data: &str) -> i32 {
     .map(|v| v.map(|vals|score(&vals.0, &vals.1)))
     .collect();
     
-    match res {
-        Ok(v) => v.iter().sum(),
-        Err(e) => {
-            println!("{}", e);
-            0
-        }
-    }
+    res.map(|v| v.iter().sum())
 }
 
-fn solve_p2(data: &str) -> i32 {
-    todo!()
+fn solve_p2(data: &str) -> Result<i32, String> {
+    let res: Result<Vec<i32>, String> = data.lines()
+    .map(splt_line_p2)
+    .map(|v| v.map(|vals| {
+        let myMove = find_my_move(&vals.0, &vals.1);
+        (vals.0, myMove)
+    }))
+    .map(|v| v.map(|vals| {
+        let out = game(&vals.1, &vals.0);
+        (vals.1, out)
+    }))
+    .map(|v| v.map(|vals|score(&vals.0, &vals.1)))
+    .collect();
+
+    res.map(|v| v.iter().sum())
 }
