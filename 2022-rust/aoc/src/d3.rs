e1c7d3da41b5cfe680637f3d57c78392543700de
kfigon <kamil.figon@gmail.com>
2023-07-03 13:39:43 +0200

    d3

--- 2022-rust/aoc/src/d3.rs
+++ 2022-rust/aoc/src/d3.rs
@@ -67,18 +67,41 @@ fn p1(input: &str) -> i32 {
 fn p2(input: &str) -> i32 {
     let scores = get_scores();
 
+    const limit: usize = 3;
+
     let mut res: i32 = 0;
-    for group in input.lines().collect::<Vec<&str>>().chunks(3) {
-        let mut counts: HashMap<char, i32> = HashMap::new();
+    for group in input.lines().collect::<Vec<&str>>().chunks(limit) {
+        let mut counts: HashMap<char, usize> = HashMap::new();
         group.iter()
             .for_each(|g| g.chars().collect::<HashSet<char>>().iter()
                 .for_each(|v| *counts.entry(*v).or_insert(0) += 1)
             );
 
-        res += counts.iter().filter(|v| *v.1 == 3)
+        res += counts.iter().filter(|v| *v.1 == limit)
             .map(|v| *scores.get(v.0).unwrap() as i32)
             .sum::<i32>();
     }
 
     res
+
+
+    // let mut res: i32 = 0;
+    // let lines: Vec<&str> = input.lines().collect();
+    
+    // for i in (0..lines.len()).step_by(3) {
+    //     let elf1 = lines.get(i).unwrap().chars().collect::<HashSet<char>>();
+    //     let elf2 = lines.get(i+1).unwrap().chars().collect::<HashSet<char>>();
+    //     let elf3 = lines.get(i+2).unwrap().chars().collect::<HashSet<char>>();
+
+    //     let mut counts: HashMap<char, i32> = HashMap::new();
+    //     elf1.iter().for_each(|c| *counts.entry(*c).or_insert(0) += 1 );
+    //     elf2.iter().for_each(|c| *counts.entry(*c).or_insert(0) += 1 );
+    //     elf3.iter().for_each(|c| *counts.entry(*c).or_insert(0) += 1 );
+
+    //     res += counts.iter()
+    //         .filter(|v| *v.1 == 3)
+    //         .map(|v| *scores.get(v.0).unwrap() as i32)
+    //         .sum::<i32>();
+    // }
+    // res
 }
