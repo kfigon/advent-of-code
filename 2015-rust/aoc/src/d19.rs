22d1538cf2ceefcd96c923c5307c65d7ebbe0803
kfigon <kamil.figon@gmail.com>
2023-11-18 16:30:06 +0100

    p1 done

--- 2015-rust/aoc/src/d19.rs
+++ 2015-rust/aoc/src/d19.rs
@@ -22,9 +22,8 @@ SiHSi";
 fn parse_ex() {
     assert_eq!(parse(EXAMPLE1), Ok((
         HashMap::from_iter([
-            ("H", "HO"),
-            ("H", "OH"),
-            ("O", "HH"),
+            ("H", vec!["HO", "OH"]),
+            ("O", vec!["HH"]),
         ]),
         "HOH")
     ))
@@ -49,12 +48,27 @@ fn p1_ex_3() {
 }
 
 #[test]
-fn p1_test() {
-    todo!();
-    assert_eq!(1, p1(&fs::read_to_string("d19.txt").unwrap()));
+fn substr_test() {
+    assert_eq!(substr("fooBarfoo", "foo", 0), Some(0));
+    assert_eq!(substr("fooBarfoo", "foo", 1), Some(6));
+    assert_eq!(substr("fooBarfoo", "foo", 2), Some(6));
+    assert_eq!(substr("fooBarfoo", "foo", 4), Some(6));
+    assert_eq!(substr("fooBarfoo", "Bar", 0), Some(3));
+    assert_eq!(substr("fooBarfoo", "Bar", 1), Some(3));
+    assert_eq!(substr("fooBarfoo", "Bar", 3), Some(3));
+    assert_eq!(substr("fooBarfoo", "Bar", 2), Some(3));
+    
+    assert_eq!(substr("fooBarfoo", "Bar", 4), None);
+    assert_eq!(substr("fooBarfoo", "bar", 0), None);
+    assert_eq!(substr("fooBarfoo", "foo", 7), None);
 }
 
-fn parse(s: &str) -> Result<(HashMap<&str, &str>, &str), &'static str> {
+#[test]
+fn p1_test() {
+    assert_eq!(518, p1(&fs::read_to_string("d19.txt").unwrap()));
+}
+
+fn parse(s: &str) -> Result<(HashMap<&str, Vec<&str>>, &str), &'static str> {
     let parts = s.split_once("\n\n");
     let (mappings, formula) = match parts {
         Some(v) => (v.0,v.1),
@@ -63,45 +77,51 @@ fn parse(s: &str) -> Result<(HashMap<&str, &str>, &str), &'static str> {
 
     let mappings = mappings.lines()
         .map(|line| line.split_once(" => "))
-        .collect::<Option<HashMap<&str,&str>>>()
+        .collect::<Option<Vec<(&str,&str)>>>()
         .ok_or("invalid mapping found")?;
 
-    Ok((mappings, formula))
+    let out = mappings.iter()
+        .fold(HashMap::new(), |mut acc, v| {
+            let mut e: &mut Vec<&str> = acc.entry(v.0).or_default();
+            e.push(v.1);
+            acc
+        });
+    Ok((out, formula))
 }
 
 fn p1(s: &str) -> usize {
     let (mappings, formula) = parse(s).unwrap();
     let mut unique: HashSet<String> = HashSet::new();
 
-    for (&k,&v) in &mappings {        
-        let mut i = 0;
-        while i < formula.len() {
-            i = match substr(formula, k, i) {
-                Ok(found_idx) => {
-                    let mut molecule = String::new();
-                    molecule.push_str(&formula[..i]);
-                    molecule.push_str(v);
-                    molecule.push_str(&formula[i+k.len()..]);
-
-
-                    unique.insert(molecule);
-                    found_idx+1
-                },
-                Err(next_idx) => next_idx,
-            };
+    for (&k,vals) in &mappings {
+        for &v in vals {
+            let mut i = 0;
+            while i < formula.len() {
+                i = match substr(formula, k, i) {
+                    Some(found_idx) => {
+                        let mut molecule = String::new();
+                        molecule.push_str(&formula[..found_idx]);
+                        molecule.push_str(v);
+                        molecule.push_str(&formula[found_idx+k.len()..]);
+                        
+                        unique.insert(molecule);
+                        found_idx+1
+                    },
+                    None => break,
+                };
+            }
         }
     }
-
     unique.len()
 }
 
-fn substr(s: &str, sub: &str, start_idx: usize) -> Result<usize, usize> {
+fn substr(s: &str, sub: &str, start_idx: usize) -> Option<usize> {
     let mut i = start_idx;
     while i < s.len() {
         match s.get(i..i+sub.len()) {
-            Some(v) if v == sub => return Ok(i),
+            Some(v) if v == sub => return Some(i),
             _ => i+=1,
         };
     }
-    Err(i)
+    None
 }
