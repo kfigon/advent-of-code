5e46ffe86e387a62d8c3146fef8da5b43a344582
kfigon <kamil.figon@gmail.com>
2026-03-26 23:54:32 +0100

    d2p2

--- 2018-rust/aoc_2018/src/d2.rs
+++ 2018-rust/aoc_2018/src/d2.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::{HashMap, HashSet}, fmt::format};
 
 fn p1(input: &str) -> i32 {
     let count_freqs = |l: &str| -> HashMap<char, i32> {
@@ -24,8 +24,25 @@ fn p1(input: &str) -> i32 {
     x.0 * x.1
 }
 
-fn p2(input: &str) -> Result<i32, String> {
-    todo!()
+fn p2(input: &str) -> Option<String> {
+    let lines: Vec<&str> = input.lines().collect();
+    if lines.is_empty() {
+        return None;
+    }
+    let first_len = lines.get(0).unwrap().len(); // byte size, good enought
+
+    // check horizontal
+    for c_idx in 0..first_len {
+        let mut set: HashSet<String> = HashSet::new();
+        for line in &lines {
+        
+           let mut modified_line = format!("{}{}", &line[..c_idx],  &line[c_idx+1..]);
+           if !set.insert(modified_line.clone()) {
+            return Some(modified_line);
+           }
+        }    
+    }
+    None
 }
 
 #[cfg(test)]
@@ -50,5 +67,22 @@ ababab";
     fn p1_test() {
         let exp = 6150;
         assert_eq!(exp, p1(&fs::read_to_string("src/d2.txt").unwrap()));
+    }
+    
+    #[test]
+    fn p2_ex() {
+        let data="abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz";
+        assert_eq!(Some("fgij".to_string()), p2(data));
+    }
+
+    #[test]
+    fn p2_test() {
+        assert_eq!(Some("rteotyxzbodglnpkudawhijsc".to_string()), p2(&fs::read_to_string("src/d2.txt").unwrap()));
     }
 }
