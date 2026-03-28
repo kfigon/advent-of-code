a4904a4723229776240993c5ab4e181528ceee65
kfigon <kamil.figon@gmail.com>
2026-03-28 11:15:13 +0100

    better solution

--- 2018-rust/aoc_2018/src/d3.rs
+++ 2018-rust/aoc_2018/src/d3.rs
@@ -1,8 +1,12 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::{HashMap, HashSet, hash_map::Entry::{Occupied, Vacant}}, str::FromStr};
+
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+struct ID(i32);
 
 #[derive(Debug, PartialEq)]
 struct Descriptor {
-    id: i32,
+    id: ID,
     location: Location,
     size: Size,
 }
@@ -46,53 +50,52 @@ impl FromStr for Descriptor {
         let width: i32 = parts.get(3).ok_or(format!("missing width in {}", s))?.parse().map_err(|err|format!("failed parsing width for {}: {}", s, err))?;
         let height: i32 = parts.get(4).ok_or(format!("missing height in {}", s))?.parse().map_err(|err|format!("failed parsing height for {}: {}", s, err))?;
 
-        Ok(Descriptor{ id, location: Location { x, y }, size: Size { width, height } })
+        Ok(Descriptor{ id: ID(id), location: Location { x, y }, size: Size { width, height } })
     }
 }
 
-fn build_fabric(descs: &Vec<Descriptor>) -> HashMap<Location, i32> {
-    let mut fabric: HashMap<Location, i32> = HashMap::new();
+type FirstID = ID;
+fn build_fabric(descs: &Vec<Descriptor>) -> (HashMap<Location, (i32, FirstID)>, HashSet<ID>) {
+    let mut overlap_counts: HashMap<Location, (i32, FirstID)> = HashMap::new();
+
+    let mut all_ids = HashSet::<ID>::new();
+    let mut overlapped = HashSet::<ID>::new();
 
     for v in descs {
+        all_ids.insert(v.id);
         for loc in v.iter() {
-            fabric
-                .entry(loc.clone())
-                .and_modify(|vc| { *vc +=1; } )
-                .or_insert(1);
+            match overlap_counts.entry(loc) {
+                Occupied(mut occupied_entry) => {
+                    let (cnt, first_id) = occupied_entry.get_mut();
+                    overlapped.insert(v.id);
+                    overlapped.insert(*first_id);
+                    *cnt+=1;
+                },
+                Vacant(vacant_entry) => {
+                    vacant_entry.insert((1, v.id));
+                },
+            }
         }
     }
 
-    fabric
+    let non_overlapped = all_ids.difference(&overlapped).copied().collect();
+    (overlap_counts, non_overlapped)
 }
 
 fn p1(input: &str) -> Result<i32, String> {
     let descs = input.lines().map(&str::parse::<Descriptor>).collect::<Result<Vec<_>, _>>()?;
-    let fabric = build_fabric(&descs);
-    Ok(fabric.into_values().filter(|&v| v > 1).count() as i32)
+    let (fabric, _) = build_fabric(&descs);
+    Ok(fabric.into_values().filter(|v| v.0 > 1).count() as i32)
 }
 
-fn p2(input: &str) -> Result<i32, String> {
+fn p2(input: &str) -> Result<ID, String> {
     let descs = input.lines().map(&str::parse::<Descriptor>).collect::<Result<Vec<_>, _>>()?;
-    let fabric = build_fabric(&descs);
-    
-    for v in descs {
-        let mut allOnes = true;
-        for w in v.location.x..v.location.x + v.size.width{
-            for h in v.location.y..v.location.y + v.size.height{
-                match fabric.get(&Location { x: w, y: h }) {
-                    Some(&a) if a != 1 => {
-                        allOnes = false;
-                        break;
-                    }
-                    _ => continue,
-                }
-            }
-        }
-        if allOnes {
-            return Ok(v.id);
-        }
+    let (_, non_overlapping) = build_fabric(&descs);
+
+    match non_overlapping.iter().next() {
+        Some(id) => Ok(*id),
+        None => Err("Not found".to_string()),
     }
-    Err("not found".to_string())
 }
 
 #[cfg(test)]
@@ -103,9 +106,9 @@ mod test {
     
     #[test]
     fn parser_test() {
-        assert_eq!(Ok(Descriptor{ id: 1, location: Location { x: 1, y: 3 }, size: Size { width: 4, height: 4 }}), "#1 @ 1,3: 4x4".parse::<Descriptor>());
-        assert_eq!(Ok(Descriptor{ id: 2, location: Location { x: 3, y: 1 }, size: Size { width: 4, height: 4 }}), "#2 @ 3,1: 4x4".parse::<Descriptor>());
-        assert_eq!(Ok(Descriptor{ id: 3, location: Location { x: 5, y: 5 }, size: Size { width: 2, height: 2 }}), "#3 @ 5,5: 2x2".parse::<Descriptor>());
+        assert_eq!(Ok(Descriptor{ id: ID(1), location: Location { x: 1, y: 3 }, size: Size { width: 4, height: 4 }}), "#1 @ 1,3: 4x4".parse::<Descriptor>());
+        assert_eq!(Ok(Descriptor{ id: ID(2), location: Location { x: 3, y: 1 }, size: Size { width: 4, height: 4 }}), "#2 @ 3,1: 4x4".parse::<Descriptor>());
+        assert_eq!(Ok(Descriptor{ id: ID(3), location: Location { x: 5, y: 5 }, size: Size { width: 2, height: 2 }}), "#3 @ 5,5: 2x2".parse::<Descriptor>());
     }
 
     #[test]
@@ -126,11 +129,11 @@ mod test {
         let data = "#1 @ 1,3: 4x4
 #2 @ 3,1: 4x4
 #3 @ 5,5: 2x2";
-        assert_eq!(Ok(3), p2(data));
+        assert_eq!(Ok(ID(3)), p2(data));
     }
 
     #[test]
     fn p2_test() {
-       assert_eq!(Ok(382), p2(&fs::read_to_string("d3.txt").unwrap()));
+       assert_eq!(Ok(ID(382)), p2(&fs::read_to_string("d3.txt").unwrap()));
     }
 }
