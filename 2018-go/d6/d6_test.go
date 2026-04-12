5933d2f87a264518650af723b281f9c27ff42bcc
kfigon <kamil.figon@gmail.com>
2026-04-12 22:01:02 +0200

    p1 done

--- 2018-go/d6/d6_test.go
+++ 2018-go/d6/d6_test.go
@@ -2,6 +2,8 @@ package d6
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"strconv"
 	"strings"
 	"testing"
@@ -23,6 +25,16 @@ func TestP1(t *testing.T) {
 		require.NoError(t, err)
 
 		assert.Equal(t, 17, p1(d))
+	})
+
+	t.Run("file", func(t *testing.T) {
+		f, err := os.ReadFile("d6.txt")
+		require.NoError(t, err)
+		d, err := parse(string(f))
+		require.NoError(t, err)
+
+		got := p1(d)
+		assert.Equal(t, 4284, got)
 	})
 }
 
@@ -53,5 +65,85 @@ func parse(data string) ([]coord, error) {
 }
 
 func p1(c []coord) int {
-	return 0
+	start, end := findMapSize(c)
+	type distance struct {
+		len        int
+		coordIdx   int
+		hadConfict bool
+	}
+
+	areas := map[coord]int{}
+	infinite := map[coord]bool{}
+
+	for x := start.x; x <= end.x; x++ {
+		for y := start.y; y <= end.y; y++ {
+
+			var e *distance
+			this := coord{x, y}
+
+			for i, v := range c {
+
+				thisDistance := mahnattanDistance(this, v)
+
+				if e == nil {
+					e = &distance{thisDistance, i, false}
+				} else if thisDistance < e.len {
+					e = &distance{thisDistance, i, false}
+				} else if e.len == thisDistance {
+					e = &distance{thisDistance, i, true}
+				}
+			}
+
+			if e != nil && !e.hadConfict {
+				cell := c[e.coordIdx]
+				areas[cell]++
+
+				onEdge := x <= start.x || x >= end.x || y <= start.y || y >= end.y
+				if onEdge {
+					infinite[cell] = true
+				}
+			}
+		}
+	}
+
+	maxLen := 0
+	for candidate, v := range areas {
+		if infinite[candidate] {
+			continue
+		}
+		maxLen = max(maxLen, v)
+	}
+
+	return maxLen
+}
+
+func findMapSize(c []coord) (start coord, end coord) {
+	var zero coord
+	if len(c) < 1 {
+		return zero, zero
+	}
+
+	var minX *int
+	var minY *int
+	var maxX *int
+	var maxY *int
+	for _, v := range c {
+		if minX == nil || v.x < *minX {
+			minX = &v.x
+		}
+		if minY == nil || v.y < *minY {
+			minY = &v.y
+		}
+		if maxX == nil || v.x > *maxX {
+			maxX = &v.x
+		}
+		if maxY == nil || v.y > *maxX {
+			maxY = &v.y
+		}
+	}
+	return coord{x: *minX, y: *minY}, coord{x: *maxX, y: *maxY}
+}
+
+func mahnattanDistance(a, b coord) int {
+	return int(math.Abs(float64(a.x-b.x)) + math.Abs(float64(a.y-b.y)))
 }
